f97a48f4339234fa84b246cae4f9b0488a16bcbf
Влад Теут <vladteut@Teutss-Air.localdomain>
2024-09-05 15:19:17 +0300

    taskSumFix

--- src/components/Sum/Sum.jsx
+++ src/components/Sum/Sum.jsx
@@ -1,1 +1,16 @@
 // export const Sum = () => ();
+// export const Sum = () => ();
+
+import React from 'react';
+
+export const Sum = ({ a, b }) => {
+  const copyA = a !== undefined ? a : 0;
+  const copyB = b !== undefined ? b : 0;
+  const result = copyA + copyB;
+
+  return (
+    <p>
+      Sum of {copyA} and {copyB} is {result}
+    </p>
+  );
+};
